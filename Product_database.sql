5d3cd756cb74581c861bff4df73edfada43350c4
16Shubhangi <94099125+16Shubhangi@users.noreply.github.com>
2021-12-14 22:28:30 +0530

    Delete Product_database.sql

--- Product_database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE product1(
-  `product_code` varchar(10) NOT NULL,
-  `product_name` varchar(30) NOT NULL,
-  `qty` varchar(100) NOT NULL,
-  `price` float(6,2) NOT NULL,
-  `img` blob NOT NULL
-);
