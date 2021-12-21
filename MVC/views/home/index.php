8a1ebd406fcd3f2dee953be4257adbff7326177f
16Shubhangi <94099125+16Shubhangi@users.noreply.github.com>
2021-12-21 16:57:50 +0530

    Add files via upload

---
+++ MVC/views/home/index.php
@@ -0,0 +1,6 @@
+<div class="text-center">
+	<h1>Lets explore new Produts</h1>
+	<p class="lead"> So here we can find variety of products, <br>
+	here you will get to see or add the latest products in 2021</p>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>products">Add Now</a>
+</div>
