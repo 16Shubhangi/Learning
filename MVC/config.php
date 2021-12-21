b6a329d6c6221bad9b433d4aa612b79f4888624f
16Shubhangi <94099125+16Shubhangi@users.noreply.github.com>
2021-12-21 16:50:47 +0530

    Add files via upload

---
+++ MVC/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "Products_database");
+
+// Define URL
+define("ROOT_PATH", "/Products_database/");
+define("ROOT_URL", "http://localhost/Products_database/");
+?>
